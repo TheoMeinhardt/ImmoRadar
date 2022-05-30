15350ee3cc7bd4c5769979d4516865d0443e595e
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-05-30 10:14:34 +0200

    added client + mapbox setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div class="text-center">
+    <div class="d-flex justify-content-center"><RouterView /></div>
+  </div>
+</template>
+
+<script setup>
+import {  RouterView } from 'vue-router';
+</script>
