e20ee064e1743746b181706c9950a993a89125f6
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-05-30 11:56:36 +0200

    client ready for dev

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,59 @@
 <template>
-  <div class="container">
-    <p>Pssst. This is the <span class="text-weight-bold">Home</span> view</p>
-    <q-parallax src="@/assets/images/wien.webp">
-      <h1 class="text-white">Wien</h1>
-    </q-parallax>
-  </div>
+  <div id="map"></div>
 </template>
+
+<script>
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { onMounted, createApp, defineComponent, nextTick } from 'vue';
+import TestPopupContent from '@/components/TestPopupContent.vue';
+
+export default {
+  setup() {
+    onMounted(() => {
+      mapboxgl.accessToken = 'api key';
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/light-v9',
+      });
+      map.on('load', () => {
+        map.addSource('usa', {
+          type: 'geojson',
+          data: 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries/USA.geo.json',
+        });
+        map.addLayer({
+          id: 'usa-fill',
+          type: 'fill',
+          source: 'usa',
+          paint: {
+            'fill-color': 'red',
+          },
+        });
+        map.on('click', 'usa-fill', function (e) {
+          new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML('<div id="popup-content"></div>')
+            .addTo(map);
+          const MyNewPopup = defineComponent({
+            extends: TestPopupContent,
+            setup() {
+              const title = 'This is the USA';
+              return { title };
+            },
+          });
+          nextTick(() => {
+            createApp(MyNewPopup).mount('#popup-content');
+          });
+        });
+      });
+    });
+    return {};
+  },
+};
+</script>
+
+<style>
+#map {
+  height: 100vh;
+}
+</style>
