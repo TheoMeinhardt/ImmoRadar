de6159699001859bc9f1491fceaa8007a41a6af1
Max Schuller <schuller.m04@htlwienwest.at>
2022-09-25 16:23:31 +0200

    BasicStripe

--- server/src/config/server.ts
+++ server/src/config/server.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import Stripe from 'stripe';
 
 dotenv.config();
 
 const server = express();
 
 const PORT = process.env.PORT ?? 3000;
+
+const stripe = new Stripe(process.env.SECRET_KEY);
 
 server.use(morgan('dev'));
 server.use(helmet());
