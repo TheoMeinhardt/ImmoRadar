3812e6e32c0db84b9ff6a294bade8c8d7e9f45c0
Max Schuller <schuller.m04@htlwienwest.at>
2022-09-28 20:21:34 +0200

    Webhoo_fast_fertig

--- server/src/api/routes/realEstate.ts
+++ server/src/api/routes/realEstate.ts
@@ -1,7 +1,7 @@
 import asyncHandler from 'express-async-handler';
-import { Router } from 'express';
+import { Router, raw } from 'express';
 
-import { realEstateControllers } from '../controllers';
+import { realEstateControllers, postToWebhook } from '../controllers/realEstate';
 
 const router = Router();
 
@@ -18,5 +18,8 @@ router.patch('/:id', asyncHandler(realEstateControllers.patchRealEstate));
 
 // DELETEs
 router.delete('/:id', asyncHandler(realEstateControllers.deleteRealEstate));
+
+// STRIPE
+router.post('/webhook', raw({ type: 'application/json' }), asyncHandler(postToWebhook));
 
 export default router;
