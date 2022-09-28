bdebbb915bf39017d625d2096e9b6b775f5b7340
Max Schuller <schuller.m04@htlwienwest.at>
2022-09-28 20:21:34 +0200

    Webhoo_fast_fertig

--- server/src/api/controllers/realEstate.ts
+++ server/src/api/controllers/realEstate.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from 'express';
-
+import express from 'express';
+import Stripe from 'stripe';
+import dotenv from 'dotenv';
 import { realEstate } from '../types';
 import * as db from '../models';
+
+dotenv.config();
+
+const stripe: Stripe = new Stripe(process.env.SECRET_KEY, {
+  apiVersion: '2022-09-28',
+  appInfo: {
+    name: 'immoradar',
+    version: '0.0.1',
+    url: 'immoradar.at',
+  },
+});
 
 // Controller for sending all Real Estates to the client
 async function getAllRealEstates(req: Request, res: Response): Promise<void> {
@@ -18,4 +31,32 @@ async function getOneRealEstate(req: Request, res: Response): Promise<void> {
   else res.status(404).send('Not Found');
 }
 
-export { getAllRealEstates, getOneRealEstate };
+async function postToWebhook(req: Request, res: Response): Promise<void> {
+  express.raw({ type: 'application/json' });
+  const sig = req.headers['stripe-signature'];
+  let event: any;
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, process.env.END_POINT_SECRET);
+  } catch (err: any) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  switch (event.type) {
+  case 'payment_intent.succeeded': {
+    const paymentIntent = event.data.object;
+    console.log('PaymentIntent was successful!');
+    break;
+  }
+  case 'payment_method.attached': {
+    const paymentMethod = event.data.object;
+    console.log('PaymentMethod was attached to a Customer!');
+    break;
+  }
+  default: {
+    console.log(`Unhandled event type ${event.type}`);
+  }
+  }
+  res.json({ received: true });
+}
+
+export { getAllRealEstates, getOneRealEstate, postToWebhook };
--- server/src/api/routes/realEstate.ts
+++ server/src/api/routes/realEstate.ts
@@ -1,12 +1,16 @@
 import asyncHandler from 'express-async-handler';
 import { Router } from 'express';
+import express from 'express';
 
-import { getAllRealEstates, getOneRealEstate } from '../controllers/realEstate';
+import { getAllRealEstates, getOneRealEstate, postToWebhook } from '../controllers/realEstate';
 
 const router = Router();
 
 // GETs
 router.get('/', asyncHandler(getAllRealEstates));
 router.get('/:id', asyncHandler(getOneRealEstate));
+
+// STRIPE
+router.post('/webhook', express.raw({ type: 'application/json' }), asyncHandler(postToWebhook));
 
 export default router;
