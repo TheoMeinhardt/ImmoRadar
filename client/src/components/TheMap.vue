76022b7ae26777386e64d17ef1c2b6a14b3cd832
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-09-28 16:44:16 +0200

    mapbox setup working

---
+++ client/src/components/TheMap.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { MapboxMap } from '@studiometa/vue-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+</script>
+
+<template>
+  <MapboxMap
+    style="height: 400px"
+    access-token="pk.eyJ1IjoibWF0dGhpYXNlaXNlbmh1dCIsImEiOiJjbDNqeXRiYWEwZWdxM2VvNzJpcTRlandiIn0.WeTb8gVsnnDVGQ_9fC-bwg"
+    mapStyle="mapbox://styles/matthiaseisenhut/cl8comqv0002114mvdz3z0gh3"
+  >
+  </MapboxMap>
+</template>
