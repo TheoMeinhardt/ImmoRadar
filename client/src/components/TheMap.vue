23522621477f18185cb48651a6e0e17a0517d4bc
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-09-28 16:46:15 +0200

    deleted access-token

--- client/src/components/TheMap.vue
+++ client/src/components/TheMap.vue
@@ -6,7 +6,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 <template>
   <MapboxMap
     style="height: 400px"
-    access-token="pk.eyJ1IjoibWF0dGhpYXNlaXNlbmh1dCIsImEiOiJjbDNqeXRiYWEwZWdxM2VvNzJpcTRlandiIn0.WeTb8gVsnnDVGQ_9fC-bwg"
+    access-token="..."
     mapStyle="mapbox://styles/matthiaseisenhut/cl8comqv0002114mvdz3z0gh3"
   >
   </MapboxMap>
