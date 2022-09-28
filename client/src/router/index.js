4126da2daf33c37da68cd65c1a41d208607e2322
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-09-28 17:49:39 +0200

    added subviews, inputs and buttons

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -24,12 +24,12 @@ const router = createRouter({
       component: FormView,
       children: [
         {
-          path: '/login',
+          path: '',
           name: 'login',
           component: LoginView,
         },
         {
-          path: '/register',
+          path: '/form/register',
           name: 'register',
           component: RegisterView,
         },
