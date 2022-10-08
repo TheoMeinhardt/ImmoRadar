5ad82b0c3fb01da24fca541ec7e2ec8011fb79e7
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-10-08 13:16:52 +0200

    started with HomeView

--- client/src/components/TheMap.vue
+++ client/src/components/TheMap.vue
@@ -5,9 +5,10 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 <template>
   <MapboxMap
-    style="height: 400px"
-    access-token="..."
+    style="height: 100vh"
+    access-token="pk.eyJ1IjoibWF0dGhpYXNlaXNlbmh1dCIsImEiOiJjbDNqeXRiYWEwZWdxM2VvNzJpcTRlandiIn0.WeTb8gVsnnDVGQ_9fC-bwg"
     mapStyle="mapbox://styles/matthiaseisenhut/cl8comqv0002114mvdz3z0gh3"
+    :center="[48.211652, 16.312955]"
   >
   </MapboxMap>
 </template>
