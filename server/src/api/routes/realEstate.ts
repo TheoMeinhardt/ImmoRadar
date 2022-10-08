7b859d9d10c618d09ca9a1c4303aee6ba5c264b8
Max Schuller <schuller.m04@htlwienwest.at>
2022-10-08 15:40:58 +0200

    Stripe Functions

--- server/src/api/routes/realEstate.ts
+++ server/src/api/routes/realEstate.ts
@@ -2,7 +2,14 @@ import asyncHandler from 'express-async-handler';
 import { Router } from 'express';
 import express from 'express';
 
-import { getAllRealEstates, getOneRealEstate, postToWebhook } from '../controllers/realEstate';
+import { getAllRealEstates, getOneRealEstate } from '../controllers/realEstate';
+import {
+  postToWebhook,
+  createCustomer,
+  createCheckout,
+  createPortal,
+  createSubscription,
+} from '../controllers/stripeServer';
 
 const router = Router();
 
@@ -12,5 +19,9 @@ router.get('/:id', asyncHandler(getOneRealEstate));
 
 // STRIPE
 router.post('/webhook', express.raw({ type: 'application/json' }), asyncHandler(postToWebhook));
+router.post('/create-checkout-session', asyncHandler(createCheckout));
+router.post('/create-portal-session', asyncHandler(createPortal));
+router.post('/create-customer', asyncHandler(createCustomer));
+router.post('/create-subscription', asyncHandler(createSubscription));
 
 export default router;
