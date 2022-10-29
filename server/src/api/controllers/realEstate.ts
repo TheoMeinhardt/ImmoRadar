504fe38d86c674d416c391fca31791b088847023
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2022-10-29 16:34:15 +0200

    working real estate delete

--- server/src/api/controllers/realEstate.ts
+++ server/src/api/controllers/realEstate.ts
@@ -19,9 +19,11 @@ async function getOneRealEstate(req: Request, res: Response): Promise<void> {
   else res.status(404).send('Not Found');
 }
 
+// Controller for adding a new Real Estate sent by the client
 async function addRealEstate(req: Request, res: Response): Promise<void> {
   const newRealEstate: realEstate = req.body;
 
+  // Json verification
   realEstateValidator(newRealEstate);
   if (!realEstateValidator.errors) {
     const addedRealEstate: realEstate = await db.addRealEstate(newRealEstate);
@@ -33,4 +35,17 @@ async function addRealEstate(req: Request, res: Response): Promise<void> {
   }
 }
 
-export { getAllRealEstates, getOneRealEstate, addRealEstate };
+// Controller for deleting a real estate in the database by id sent by user
+async function deleteRealEstate(req: Request, res: Response): Promise<void> {
+  const reID: string = req.params.id;
+
+  if (await db.getOneRealEstate(reID)) {
+    const deletedRealEstate: realEstate = await db.deleteRealEstate(reID);
+    if (!deleteRealEstate) res.status(500).end();
+    res.status(200).send(`deleted real estate "${deletedRealEstate.name}"`);
+  } else {
+    res.status(404).send(`real estate with id "${reID}" does not exist`);
+  }
+}
+
+export { getAllRealEstates, getOneRealEstate, addRealEstate, deleteRealEstate };
