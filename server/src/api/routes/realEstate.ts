2a60f9b37d6d241c9d50212fd7ddf84ff1535c8a
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2022-10-29 19:37:16 +0200

    working get shortend real estates

--- server/src/api/routes/realEstate.ts
+++ server/src/api/routes/realEstate.ts
@@ -1,12 +1,13 @@
 import asyncHandler from 'express-async-handler';
 import { Router } from 'express';
 
-import { getAllRealEstates, getOneRealEstate, addRealEstate, deleteRealEstate, patchRealEstate } from '../controllers/realEstate';
+import { getAllRealEstates, getOneRealEstate, getShortendRealEstates, addRealEstate, deleteRealEstate, patchRealEstate } from '../controllers/realEstate';
 
 const router = Router();
 
 // GETs
 router.get('/', asyncHandler(getAllRealEstates));
+router.get('/short', asyncHandler(getShortendRealEstates));
 router.get('/:id', asyncHandler(getOneRealEstate));
 
 // POSTs
