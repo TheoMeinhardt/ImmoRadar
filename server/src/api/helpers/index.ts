0806ac379b09854dd8b463ec5a64335653efc0f4
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2022-10-31 16:43:01 +0100

    added check if user exists helper

--- server/src/api/helpers/index.ts
+++ server/src/api/helpers/index.ts
@@ -1,2 +1,4 @@
+export { default as userExists } from './checkIfUserExists';
+
 export { default as realEstateMapper } from './mapper/realEstate';
 export { default as userMapper } from './mapper/user';
