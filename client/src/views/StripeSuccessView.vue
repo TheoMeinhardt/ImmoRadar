ba864fabd53f6fc9580e87c06e8721c875f5f8d4
Max Schuller <schuller.m04@htlwienwest.at>
2022-11-02 16:04:19 +0100

    Frontend Views

---
+++ client/src/views/StripeSuccessView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="togethere-background"></div>
+    <div class="sr-root">
+      <div class="sr-main">
+        <div class="sr-payment-summary completed-view">
+          <h1>Your payment succeeded</h1>
+          <h4>View CheckoutSession response:</h4>
+        </div>
+        
+      </div>
+      <div class="sr-content"></div>
+    </div>
+  </div>
+</template>
