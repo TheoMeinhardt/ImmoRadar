7bcc9dea942ad3a23c0445542b96b07a2c5bde54
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-11-07 19:16:55 +0100

    working on NavBar

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="xs">
-    <TheMap></TheMap>
+    <!-- <TheMap></TheMap> -->
+    <NavBar></NavBar>
   </div>
 </template>
 
 <script setup>
 import TheMap from '../components/TheMap.vue';
+import NavBar from '../components/NavBar.vue';
 </script>
