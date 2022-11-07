77d200a1e698a3c89988e0dba0a357b05e5de721
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-11-07 19:16:55 +0100

    working on NavBar

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul class="row ">
+      <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+      <i class="fa-solid fa-house fa-xl"></i>
+      <i class="fa-solid fa-user fa-xl"></i>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: #71d1ec;
+  width: 55vw;
+  height: 4.5vh;
+  border-radius: 25px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  align-items: center;
+}
+
+i {
+  color: #4b506e;
+}
+</style>
--- client/src/components/TheMap.vue
+++ client/src/components/TheMap.vue
@@ -9,6 +9,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
     access-token="pk.eyJ1IjoibWF0dGhpYXNlaXNlbmh1dCIsImEiOiJjbDNqeXRiYWEwZWdxM2VvNzJpcTRlandiIn0.WeTb8gVsnnDVGQ_9fC-bwg"
     mapStyle="mapbox://styles/matthiaseisenhut/cl8comqv0002114mvdz3z0gh3"
     :center="[48.211652, 16.312955]"
+    zoom="12"
   >
   </MapboxMap>
 </template>
