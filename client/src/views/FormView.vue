658b51e2b7ef024b0fed2f5afb994371d1580d54
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-11-07 15:25:47 +0100

    LoginView fast fertig

---
+++ client/src/views/FormView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="body" class="xs">
+    <h4 style="font-family: Quicksand" class="text-white text-center">ImmoRadar</h4>
+    <q-tabs style="font-family: Keep Calm" class="text-white">
+      <q-route-tab to="/form">Login</q-route-tab>
+      <q-route-tab to="/form/register">Register</q-route-tab>
+    </q-tabs>
+    <RouterView class="q-mt-xl"></RouterView>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+#body {
+  height: 100vh;
+  background-color: #4b506e;
+}
+</style>
---
+++ client/src/views/subviews/RegisterView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <q-input
+      bg-color="white"
+      style="font-family: Keep Calm"
+      class="q-my-lg q-mx-sm"
+      rounded
+      outlined
+      v-model="username"
+      label="Username"
+    />
+    <q-input
+      class="q-mx-sm"
+      bg-color="white"
+      style="font-family: Keep Calm"
+      rounded
+      outlined
+      v-model="email"
+      label="Email"
+      type="email"
+      error="true"
+      error-message="Please enter a valid email address"
+    />
+    <q-input
+      bg-color="white"
+      style="font-family: Keep Calm"
+      class="q-my-lg q-mx-sm"
+      rounded
+      outlined
+      v-model="phone"
+      label="Phone Nr."
+      mask="### #### ####"
+      prefix="+43"
+      type="tel"
+    />
+    <q-input
+      bg-color="white"
+      style="font-family: Keep Calm"
+      class="q-my-lg q-mx-sm"
+      rounded
+      outlined
+      v-model="password"
+      label="Password"
+      type="password"
+    />
+    <q-btn
+      unelevated
+      rounded
+      color="light-blue-3"
+      style="font-family: Keep Calm; width: 300px; content-align: center"
+      label="Login"
+    />
+  </div>
+</template>
+
+<script setup></script>
