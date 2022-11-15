5414edead0a40f799c21f856ff03822408251fc2
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2022-11-15 18:19:31 +0100

    fixed errors and style errors

--- client/src/views/FormView.vue
+++ client/src/views/FormView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="xs bg-blue-grey-7 container">
-    <div style="text-align: center;"><img class="q-my-md" src="/images/Logo_Horizontal.svg" alt="" width="250"></div>
+  <div class="xs container" style="background-color: #4b506e">
+    <div style="text-align: center"><img class="q-my-md" src="/images/Logo_Horizontal.svg" alt="" width="250" /></div>
     <q-tabs style="font-family: Keep Calm" class="text-white">
       <q-route-tab to="/form">Login</q-route-tab>
       <q-route-tab to="/form/register">Register</q-route-tab>
