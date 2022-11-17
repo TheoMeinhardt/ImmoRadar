6ccf0f3e4f0287d71e3c8a7ffb8eae21cfd25376
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2022-11-17 08:47:37 +0100

    changed vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://10.0.0.58:3000',
+        target: 'http://127.0.0.1:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace('/api', ''),
       },
