44b869857069d0be82102e539ef6252d9e2b9ab6
Matthias Eisenhut <eisenhut.m03@htlwienwest.at>
2022-11-21 14:20:02 +0100

    added UnderConstruction + user Route and search Route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
+import UserView from '@/views/UserView.vue';
+import SearchView from '@/views/SearchView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserView,
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: SearchView,
     },
   ],
 });
---
+++ client/src/views/UserView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="xs text-center" style="vertical-align: center" id="underConstruction">
+    <UnderConstruction></UnderConstruction>
+    <NavBar></NavBar>
+  </div>
+</template>
+
+<script setup>
+import UnderConstruction from '@/components/UnderConstruction.vue';
+import NavBar from '@/components/NavBar.vue';
+</script>
+
+<style scoped>
+#underConstruction {
+  height: 100vh;
+  background-color: #4b506e;
+}
+</style>
