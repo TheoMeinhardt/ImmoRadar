7a7543bf46bcde15f27448b48c1b47ad0846a84c
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2022-12-06 17:28:28 +0100

    Bugfix

--- client/src/components/TheMap.vue
+++ client/src/components/TheMap.vue
@@ -21,8 +21,8 @@ const realEstates = ref();
 // const badgeExpanded = ref(false);
 
 async function getShortRealEstates() {
-  const { data } = await axios.get('https://immoradar-server.onrender.com/realestate/short');
   // const { data } = await axios.get('/api/realestate/short');
-  realEstates.value = data;
+  // const { data } = await axios.get('/api/realestate/short');
+  // realEstates.value = data;
 }
 </script>
