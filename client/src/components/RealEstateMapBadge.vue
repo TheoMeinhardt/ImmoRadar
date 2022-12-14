73ec29abadcff143d2594b6f380d4d457a9d777d
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2022-12-14 13:29:09 +0100

    badge close button & style enhancements

--- client/src/components/RealEstateMapBadge.vue
+++ client/src/components/RealEstateMapBadge.vue
@@ -2,14 +2,18 @@
   <div>
     <q-icon @click="expand = true" v-if="!expand" class="shadow-1" name="fa-solid fa-house" color="primary" size="xs"></q-icon>
 
-    <div @click="expand = false" v-else class="badge bg-secondary q-pa-sm text-white">
-      <img src="https://via.placeholder.com/200x150" />
-      <span class="text-h5 block">{{ realEstate.name }}</span>
-      <span class="text-caption block">{{ realEstate.address.split(',')[0] }}</span>
-      <span class="text-caption block seperator">{{ realEstate.address.split(',')[1] }}</span>
-      <span class="text-body1 block q-mt-sm">{{ realEstate.price }}€</span>
-      <span class="text-body1 block">{{ realEstate.usableArea }}m²</span>
-      <span class="text-body1 block">{{ realEstate.rooms }} Rooms</span>
+    <div v-else>
+      <q-btn @click="expand = false" round color="primary" class="closeIcon" icon="fa-solid fa-close" text-color="white" />
+
+      <div class="badge bg-secondary q-pa-sm q-ma-md text-white">
+        <img src="https://via.placeholder.com/200x150" />
+        <span class="text-h5 block">{{ realEstate.name }}</span>
+        <span class="text-caption block">{{ realEstate.address.split(',')[0] }}</span>
+        <span class="text-caption block seperator">{{ realEstate.address.split(',')[1] }}</span>
+        <span class="text-body1 block q-mt-sm">{{ realEstate.price }}€</span>
+        <span class="text-caption block">{{ realEstate.usableArea }}m²</span>
+        <span class="text-caption block">{{ realEstate.rooms }} Rooms</span>
+      </div>
     </div>
   </div>
 </template>
@@ -30,6 +34,12 @@ defineProps({
   border-radius: 15px;
   font-family: 'Keep Calm';
   width: 220px;
+}
+
+.closeIcon {
+  position: absolute;
+  right: 0px;
+  top: -3px;
 }
 
 img {
