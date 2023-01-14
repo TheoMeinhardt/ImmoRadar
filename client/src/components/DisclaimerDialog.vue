30d77ef0b09cea1f3164be810ea18e6fe9eb5e19
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2023-01-14 17:02:02 +0100

    added disclaimer to site

---
+++ client/src/components/DisclaimerDialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-dialog v-model="showDisclaimer">
+    <q-card class="bg-secondary text-white">
+      <q-card-section>
+        <div class="text-h6">Please read before continuing!</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">ImmoRadar is still in a very early stage of development! Because of this, some features may not work as expected or do not work at all. Futhermore, ImmoRadar receives weekly updates, which fix issues or implement new features. Those may or may not result in different unexpected behaviour as before. Please keep this in mind while using our Application!</q-card-section>
+      <q-card-section class="q-pt-none">Feel free to check out our <a href="https://linktr.ee/ImmoRadar" class="text-primary">Linktree</a> for development updates!</q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="OK" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showDisclaimer = ref(true);
+</script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -9,12 +9,14 @@
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
       <q-btn class="q-mb-xl" fab icon="fa-solid fa-house-chimney-medical" color="primary" to="/addhouse"></q-btn>
     </q-page-sticky>
+    <Disclaimer></Disclaimer>
   </q-layout>
 </template>
 
 <script setup>
 import TheMap from '../components/TheMap.vue';
 import NavBar from '../components/NavBar.vue';
+import Disclaimer from '../components/DisclaimerDialog.vue';
 </script>
 
 <style>
