432995c8fde68e182af4893d15e9f1ed6e904213
Max Schuller <schuller.m04@htlwienwest.at>
2023-01-16 22:02:29 +0100

    Stripe Datenbank Funktionen

--- server/src/api/controllers/session.js
+++ server/src/api/controllers/session.js
@@ -1,16 +1,28 @@
-import { dbGetSession, dbPostSession } from '../models/session.js';
+import { dbGetSessionID, dbPostSessionID, dbDeleteSessionID } from '../models/session.js';
 
-const getSession = async (req, res) => {
+const getSessionID = async (req, res) => {
   const { id } = req.params;
-  const session = await dbGetSession(id);
-  if (!session) return res.status(404).send('Ressource not found');
-  return res.status(200).json(session);
+  const user_id = id;
+  const sessionID = await dbGetSessionID(user_id);
+  if (!sessionID) return res.status(404).send('Ressource not found');
+  return res.status(200).json(sessionID);
 };
 
-const postSession = async (req, res) => {
+const postSessionID = async (req, res) => {
+  const { id } = req.params;
+  const user_id = id;
   if (!req.body) return res.status(400).json('error');
-  const session = await dbPostSession(req.body);
-  return res.status(200).json(session);
+  const sessionID = await dbPostSessionID(req.body, user_id);
+  return res.status(200).json(sessionID);
 };
 
-export { getSession, postSession };
+const deleteSessionID = async (req, res) => {
+  const { id } = req.params;
+  const user_id = id;
+  const sessionID = await dbGetSessionID(user_id);
+  if (sessionID == null) return res.status(404).send('User does not have session id!');
+  const deltedSessionID = await dbDeleteSessionID(user_id);
+  return res.status(200).json(deltedSessionID);
+};
+
+export { getSessionID, postSessionID, deleteSessionID };
