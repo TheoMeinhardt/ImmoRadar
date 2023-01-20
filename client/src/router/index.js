6b871a0481ae46ff2d5202ef0015d3398f3c90cf
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2023-01-20 11:51:18 +0100

    Matthias hat verschissen v1

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -15,104 +15,100 @@ import AddHouseTwo from '@/views/subviews/AddHouseTwo.vue';
 import AddHouseThree from '@/views/subviews/AddHouseThree.vue';
 import AddHouseFinal from '@/views/subviews/AddHouseFinal.vue';
 import UserOverview from '@/views/subviews/UserOverview.vue';
-import AddHouseOne from '@/views/subviews/AddHouseOne.vue';
-import AddHouseTwo from '@/views/subviews/AddHouseTwo.vue';
-import AddHouseThree from '@/views/subviews/AddHouseThree.vue';
-import AddHouseFinal from '@/views/subviews/AddHouseFinal.vue';
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
-    },
-    {
-      path: '/user',
-      name: 'user',
-      component: UserView,
-      children: [
-        {
-          path: '',
-          name: 'userOverview',
-          component: UserOverview,
-        },
-        {
-          path: '/user/details',
-          name: 'userDetails',
-          component: () => import('../views/subviews/UserDetails.vue'),
-        },
-      ],
-    },
+	history: createWebHashHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: () => import('@/views/HomeView.vue'),
+		},
+		{
+			path: '/about',
+			name: 'about',
+			component: AboutView,
+		},
+		{
+			path: '/user',
+			name: 'user',
+			component: UserView,
+			children: [
+				{
+					path: '',
+					name: 'userOverview',
+					component: UserOverview,
+				},
+				{
+					path: '/user/details',
+					name: 'userDetails',
+					component: () => import('../views/subviews/UserDetails.vue'),
+				},
+			],
+		},
 
-    {
-      path: '/search',
-      name: 'search',
-      component: SearchView,
-    },
-    {
-      path: '/addhouse',
-      name: 'addhouse',
-      component: AddHouseView,
-      children: [
-        {
-          path: '',
-          name: 'AddHouseOne',
-          component: AddHouseOne,
-        },
-        {
-          path: '/addhouse/addhousetwo',
-          name: 'AddHouseTwo',
-          component: AddHouseTwo,
-        },
-        {
-          path: '/addhouse/addhousethree',
-          name: 'AddHouseThree',
-          component: AddHouseThree,
-        },
-        {
-          path: '/addhouse/addhousefinal',
-          name: 'AddHouseFinal',
-          component: AddHouseFinal,
-        },
-      ],
-    },
-    {
-      path: '/form',
-      name: 'form',
-      component: FormView,
-      children: [
-        {
-          path: '',
-          name: 'login',
-          component: LoginView,
-        },
-        {
-          path: '/form/register',
-          name: 'register',
-          component: RegisterView,
-        },
-      ],
-    },
-    { path: '/success', name: 'success', component: SuccessView },
-    {
-      path: '/cancel',
-      name: 'cancel',
-      component: CancelView,
-    },
-    {
-      path: '/checkout',
-      name: 'checkout',
-      component: CheckoutView,
-    },
-    { path: '/:catchAll(.*)', component: NotFound },
-  ],
+		{
+			path: '/search',
+			name: 'search',
+			component: SearchView,
+		},
+		{
+			path: '/addhouse',
+			name: 'addhouse',
+			component: AddHouseView,
+			children: [
+				{
+					path: '',
+					name: 'AddHouseOne',
+					component: AddHouseOne,
+				},
+				{
+					path: '/addhouse/addhousetwo',
+					name: 'AddHouseTwo',
+					component: AddHouseTwo,
+				},
+				{
+					path: '/addhouse/addhousethree',
+					name: 'AddHouseThree',
+					component: AddHouseThree,
+				},
+				{
+					path: '/addhouse/addhousefinal',
+					name: 'AddHouseFinal',
+					component: AddHouseFinal,
+				},
+			],
+		},
+		{
+			path: '/form',
+			name: 'form',
+			component: FormView,
+			children: [
+				{
+					path: '',
+					name: 'login',
+					component: LoginView,
+				},
+				{
+					path: '/form/register',
+					name: 'register',
+					component: RegisterView,
+				},
+			],
+		},
+		{ path: '/success', name: 'success', component: SuccessView },
+		{
+			path: '/cancel',
+			name: 'cancel',
+			component: CancelView,
+		},
+		{
+			path: '/checkout',
+			name: 'checkout',
+			component: CheckoutView,
+		},
+		{ path: '/:catchAll(.*)', component: NotFound },
+	],
 });
 
 export default router;
