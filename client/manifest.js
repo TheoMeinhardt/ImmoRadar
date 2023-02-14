d9e8780107a89fa67a6a69df491d461c2e41dcfe
Max Schuller <schuller.m04@htlwienwest.at>
2023-02-14 17:50:11 +0100

    Icons

--- client/manifest.js
+++ client/manifest.js
@@ -9,7 +9,7 @@ export default {
   theme_color: '#ffffff',
   id: '/',
   icons: [
-    { src: '/images/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-    { src: '/images/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    { src: '/client/public/images/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+    { src: '/client/public/images/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
   ],
 };
