7bf9ecf5c6c0889f83b12366a63d5e0b84556c35
Max Schuller <schuller.m04@htlwienwest.at>
2023-02-20 20:03:12 +0100

    Likes Functions

--- server/src/api/controllers/commentSystem.js
+++ server/src/api/controllers/commentSystem.js
@@ -1,37 +1,45 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-await-in-loop */
 import {
-  dBgetPostsByRealEstate,
-  dBgetPostByPostID,
-  dBgetCommentsByPost,
-  dBgetCommentByCommentID,
-  dBgetLikesFromPost,
-  dBgetLikesFromComments,
-  dBpostPosts,
-  dBpostComment,
-  dBpatchPost,
-  dBpatchComment,
-  dBdeletePost,
-  dBdeleteComment,
+  dbCheckLikeExistsOnPost,
+  dbCheckLikeExistsOnComment,
+  dbGetPostsByRealEstate,
+  dbGetPostByPostID,
+  dbGetCommentsByPost,
+  dbGetCommentByCommentID,
+  dbGetLikesFromPost,
+  dbGetLikesFromComments,
+  dbPostPost,
+  dbLikePost,
+  dbPostComment,
+  dbLikeComment,
+  dbPatchPost,
+  dbPatchComment,
+  dbDeletePost,
+  dbUnlikePost,
+  dbDeleteComment,
+  dbUnlikeComment,
 } from '../models/commentSystem.js';
+
+//  -------------- GETS --------------
 
 async function getPostsAndComments(req, res) {
   const { re_id } = req.params;
-  const posts = await dBgetPostsByRealEstate(re_id);
+  const posts = await dbGetPostsByRealEstate(re_id);
 
   const postCommentResult = {};
 
   for (let i = 0; i < posts.length; i += 1) {
     const post = posts[i];
-    const comments = await dBgetCommentsByPost(post.post_id);
-    const likesForPost = await dBgetLikesFromPost(post.post_id);
+    const comments = await dbGetCommentsByPost(post.post_id);
+    const likesForPost = await dbGetLikesFromPost(post.post_id);
     if (!postCommentResult[post.post_id]) {
       postCommentResult[post.post_id] = { ...post, comments: [] };
     }
     postCommentResult[post.post_id].likes = likesForPost[0].count;
     for (let j = 0; j < comments.length; j += 1) {
       const comment = comments[j];
-      const likesForComment = await dBgetLikesFromComments(comment.comment_id);
+      const likesForComment = await dbGetLikesFromComments(comment.comment_id);
       if (
         !postCommentResult[post.post_id].comments.find((c) => c.comment_id === comment.comment_id)
       ) {
@@ -51,11 +59,13 @@ async function getPostsAndComments(req, res) {
   res.status(200).json(Object.values(postCommentResult));
 }
 
+//  -------------- POSTS --------------
+
 async function postPost(req, res) {
   try {
     const { title, content, user_id, re_id } = req.body;
 
-    const result = await dBpostPosts(title, content, user_id, re_id);
+    const result = await dbPostPost(title, content, user_id, re_id);
     res.status(200).json(result);
   } catch (err) {
     console.error(err);
@@ -67,7 +77,7 @@ async function postComment(req, res) {
   try {
     const { content, user_id, post_id } = req.body;
 
-    const result = await dBpostComment(content, user_id, post_id);
+    const result = await dbPostComment(content, user_id, post_id);
     res.status(200).json(result);
   } catch (err) {
     console.error(err);
@@ -75,11 +85,61 @@ async function postComment(req, res) {
   }
 }
 
+async function likePost(req, res) {
+  try {
+    const { user_id, post_id } = req.body;
+
+    // Check if post exists
+    const postExists = await dbGetPostByPostID(post_id);
+    if (!postExists) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    // Check if like already exists
+    const likeExists = await dbCheckLikeExistsOnPost(user_id, post_id);
+    if (likeExists) {
+      return res.status(400).json({ error: 'Like already exists' });
+    }
+
+    const result = await dbLikePost(user_id, post_id);
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+}
+
+async function likeComment(req, res) {
+  try {
+    const { user_id, comment_id } = req.body;
+
+    // Check if comment exists
+    const commentExists = await dbGetCommentByCommentID(comment_id);
+    if (!commentExists) {
+      return res.status(404).json({ error: 'Comment not found' });
+    }
+
+    // Check if like already exists
+    const likeExists = await dbCheckLikeExistsOnComment(user_id, comment_id);
+    if (likeExists) {
+      return res.status(400).json({ error: 'Like already exists' });
+    }
+
+    const result = await dbLikeComment(user_id, comment_id);
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+}
+
+//  -------------- PATCHES/UPDATES --------------
+
 async function patchPost(req, res) {
   const { title, content, user_id } = req.body;
   const { post_id } = req.params;
 
-  const post = await dBgetPostByPostID(post_id);
+  const post = await dbGetPostByPostID(post_id);
 
   if (!post) {
     return res.status(404).json({ message: 'Post not found' });
@@ -89,7 +149,7 @@ async function patchPost(req, res) {
     return res.status(401).json({ message: 'Not authorized to edit this post' });
   }
 
-  const updatedPost = await dBpatchPost(title, content, post_id);
+  const updatedPost = await dbPatchPost(title, content, post_id);
 
   return res.status(200).json(updatedPost);
 }
@@ -98,7 +158,7 @@ async function dBPatchComment(req, res) {
   const { content, user_id } = req.body;
   const { comment_id } = req.params;
 
-  const comment = await dBgetCommentByCommentID(comment_id);
+  const comment = await dbGetCommentByCommentID(comment_id);
 
   if (!comment) {
     return res.status(404).json({ message: 'Comment not found' });
@@ -108,9 +168,69 @@ async function dBPatchComment(req, res) {
     return res.status(401).json({ message: 'Not authorized to edit this comment' });
   }
 
-  const updatedComment = await dBpatchComment(content, comment_id);
+  const updatedComment = await dbPatchComment(content, comment_id);
 
   return res.status(200).json(updatedComment);
 }
 
-export { getPostsAndComments, postPost, postComment, patchPost, dBPatchComment };
+//  -------------- DELETES --------------
+
+async function unlikePost(req, res) {
+  try {
+    const { user_id, post_id } = req.body;
+
+    // Check if post exists
+    const postExists = await dbGetPostByPostID(post_id);
+    if (!postExists) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    // Check if like exists
+    const likeExists = await dbCheckLikeExistsOnPost(user_id, post_id);
+    if (!likeExists) {
+      return res.status(400).json({ error: 'Like does not exist' });
+    }
+
+    const result = await dbUnlikePost(user_id, post_id);
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+}
+
+async function unlikeComment(req, res) {
+  try {
+    const { user_id, comment_id } = req.body;
+
+    // Check if comment exists
+    const commentExists = await dbGetCommentByCommentID(comment_id);
+    if (!commentExists) {
+      return res.status(404).json({ error: 'Comment not found' });
+    }
+
+    // Check if like exists
+    const likeExists = await dbCheckLikeExistsOnComment(user_id, comment_id);
+    if (!likeExists) {
+      return res.status(400).json({ error: 'Like does not exist' });
+    }
+
+    const result = await dbUnlikeComment(user_id, comment_id);
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+}
+
+export {
+  getPostsAndComments,
+  postPost,
+  postComment,
+  patchPost,
+  dBPatchComment,
+  likePost,
+  unlikePost,
+  likeComment,
+  unlikeComment,
+};
--- server/src/api/models/commentSystem.js
+++ server/src/api/models/commentSystem.js
@@ -3,7 +3,23 @@ import { query } from '../../config/dbconfig';
 
 //  -------------- GETS --------------
 
-async function dBgetPostsByRealEstate(re_id) {
+async function dbCheckLikeExistsOnPost(user_id, post_id) {
+  const { rows } = await query('SELECT * FROM likes WHERE user_id = $1 AND post_id = $2', [
+    user_id,
+    post_id,
+  ]);
+  return rows.length > 0;
+}
+
+async function dbCheckLikeExistsOnComment(user_id, comment_id) {
+  const { rows } = await query('SELECT * FROM likes WHERE user_id = $1 AND comment_id = $2', [
+    user_id,
+    comment_id,
+  ]);
+  return rows.length > 0;
+}
+
+async function dbGetPostsByRealEstate(re_id) {
   const { rows } = await query(
     'SELECT posts.*, user.user_id ,user.name FROM posts join user on posts.user_id = user.user_id where re_id = $1',
     [re_id],
@@ -11,7 +27,7 @@ async function dBgetPostsByRealEstate(re_id) {
   return rows;
 }
 
-async function dBgetPostByPostID(post_id) {
+async function dbGetPostByPostID(post_id) {
   const { rows } = await query(
     'SELECT posts.*, user.user_id, user.name FROM posts JOIN user ON posts.user_id = user.user_id WHERE post_id = $1',
     [post_id],
@@ -19,7 +35,7 @@ async function dBgetPostByPostID(post_id) {
   return rows[0];
 }
 
-async function dBgetCommentsByPost(post_id) {
+async function dbGetCommentsByPost(post_id) {
   const { rows } = await query(
     'SELECT comments.*, user.user_id, user.name FROM comments join user on comments.user_id = user.user_id where post_id = $1',
     [post_id],
@@ -27,7 +43,7 @@ async function dBgetCommentsByPost(post_id) {
   return rows;
 }
 
-async function dBgetCommentByCommentID(comment_id) {
+async function dbGetCommentByCommentID(comment_id) {
   const { rows } = await query(
     'SELECT comments.*, user.user_id, user.name FROM comments JOIN user ON comments.user_id = user.user_id WHERE comment_id = $1',
     [comment_id],
@@ -35,19 +51,19 @@ async function dBgetCommentByCommentID(comment_id) {
   return rows[0];
 }
 
-async function dBgetLikesFromPost(post_id) {
+async function dbGetLikesFromPost(post_id) {
   const { rows } = await query('SELECT COUNT(*) FROM likes WHERE post_id = $1', [post_id]);
   return rows;
 }
 
-async function dBgetLikesFromComments(comment_id) {
+async function dbGetLikesFromComments(comment_id) {
   const { rows } = await query('SELECT COUNT(*) FROM likes WHERE comment_id = $1', [comment_id]);
   return rows;
 }
 
 //  -------------- POSTS --------------
 
-async function dBpostPosts(title, content, user_id, re_id) {
+async function dbPostPost(title, content, user_id, re_id) {
   const { rows } = await query(
     'INSERT INTO posts (title, content, user_id, re_id) VALUES ($1, $2, $3, $4) RETURNING *',
     [title, content, user_id, re_id],
@@ -55,7 +71,15 @@ async function dBpostPosts(title, content, user_id, re_id) {
   return rows;
 }
 
-async function dBpostComment(content, user_id, post_id) {
+async function dbLikePost(user_id, post_id) {
+  const { rows } = await query('INSERT INTO likes (user_id, post_id) VALUES ($1, $2) RETURNING *', [
+    user_id,
+    post_id,
+  ]);
+  return rows;
+}
+
+async function dbPostComment(content, user_id, post_id) {
   const { rows } = await query(
     'INSERT INTO comments (title, content, user_id, post_id) VALUES ($1, $2, $3, $4) RETURNING *',
     [content, user_id, post_id],
@@ -63,9 +87,17 @@ async function dBpostComment(content, user_id, post_id) {
   return rows;
 }
 
+async function dbLikeComment(user_id, comment_id) {
+  const { rows } = await query(
+    'INSERT INTO likes (user_id, comment_id) VALUES ($1, $2) RETURNING *',
+    [user_id, comment_id],
+  );
+  return rows;
+}
+
 //  -------------- PATCHES/UPDATES --------------
 
-async function dBpatchPost(title, content, post_id) {
+async function dbPatchPost(title, content, post_id) {
   const { rows } = await query(
     'update posts set title = $1, content = $2 where post_id = $3 RETURNING *',
     [title, content, post_id],
@@ -73,7 +105,7 @@ async function dBpatchPost(title, content, post_id) {
   return rows;
 }
 
-async function dBpatchComment(content, comment_id) {
+async function dbPatchComment(content, comment_id) {
   const { rows } = await query(
     'update comments set content = $1 where comment_id = $2 RETURNING *',
     [content, comment_id],
@@ -83,27 +115,49 @@ async function dBpatchComment(content, comment_id) {
 
 //  -------------- DELETES --------------
 
-async function dBdeletePost(post_id) {
+async function dbDeletePost(post_id) {
   const { rows } = await query('delete from posts where post_id = $1', [post_id]);
   return rows;
 }
 
-async function dBdeleteComment(comment_id) {
+async function dbUnlikePost(post_id, user_id) {
+  const { rows } = await query(
+    'DELETE FROM likes WHERE post_id = $1 AND user_id = $2 RETURNING *',
+    [post_id, user_id],
+  );
+  return rows;
+}
+
+async function dbDeleteComment(comment_id) {
   const { rows } = await query('delete from comments where comment_id = $1', [comment_id]);
   return rows;
 }
 
+async function dbUnlikeComment(comment_id, user_id) {
+  const { rows } = await query(
+    'DELETE FROM likes WHERE comment_id = $1 AND user_id = $2 RETURNING *',
+    [comment_id, user_id],
+  );
+  return rows;
+}
+
 export {
-  dBgetPostsByRealEstate,
-  dBgetPostByPostID,
-  dBgetCommentsByPost,
-  dBgetCommentByCommentID,
-  dBgetLikesFromPost,
-  dBgetLikesFromComments,
-  dBpostPosts,
-  dBpostComment,
-  dBpatchPost,
-  dBpatchComment,
-  dBdeletePost,
-  dBdeleteComment,
+  dbCheckLikeExistsOnPost,
+  dbCheckLikeExistsOnComment,
+  dbGetPostsByRealEstate,
+  dbGetPostByPostID,
+  dbGetCommentsByPost,
+  dbGetCommentByCommentID,
+  dbGetLikesFromPost,
+  dbGetLikesFromComments,
+  dbPostPost,
+  dbLikePost,
+  dbPostComment,
+  dbLikeComment,
+  dbPatchPost,
+  dbPatchComment,
+  dbDeletePost,
+  dbUnlikePost,
+  dbDeleteComment,
+  dbUnlikeComment,
 };
